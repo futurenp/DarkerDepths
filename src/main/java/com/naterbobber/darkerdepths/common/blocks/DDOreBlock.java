9187b23dfa7f7ae95af3d3abf1c6523509328682
0rc1nus <mitchellctlui@gmail.com>
2021-03-25 16:43:21 +0800

    Ores are fixed now, I'll leave the roots to you bg...

---
+++ src/main/java/com/naterbobber/darkerdepths/common/blocks/DDOreBlock.java
@@ -0,0 +1,38 @@
+package com.naterbobber.darkerdepths.common.blocks;
+
+import com.naterbobber.darkerdepths.core.registries.DDBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+
+import java.util.Random;
+
+public class DDOreBlock extends Block {
+
+    public DDOreBlock(Properties properties) {
+        super(properties);
+    }
+
+    protected int getExperience(Random rand) {
+        if(this == DDBlocks.ARIDROCK_COAL_ORE.get() || this == DDBlocks.LIMESTONE_COAL_ORE.get()) {
+            return MathHelper.nextInt(rand, 0, 2);
+        } else if(this == DDBlocks.ARIDROCK_LAPIS_ORE.get() || this == DDBlocks.LIMESTONE_LAPIS_ORE.get()) {
+            return MathHelper.nextInt(rand, 2, 5);
+        } else {
+            return this == DDBlocks.ARIDROCK_DIAMOND_ORE.get() || this == DDBlocks.LIMESTONE_DIAMOND_ORE.get() ? MathHelper.nextInt(rand, 3, 7) : 0;
+        }
+    }
+
+    public void spawnAdditionalDrops(BlockState state, World worldIn, BlockPos pos, ItemStack stack) {
+        super.spawnAdditionalDrops(state, worldIn, pos, stack);
+    }
+
+    @Override
+    public int getExpDrop(BlockState state, net.minecraft.world.IWorldReader reader, BlockPos pos, int fortune, int silktouch) {
+        return silktouch == 0 ? this.getExperience(RANDOM) : 0;
+    }
+}
