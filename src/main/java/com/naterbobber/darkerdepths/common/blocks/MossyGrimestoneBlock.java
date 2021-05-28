8556a65f90c865b1b5f18cf226c65f2829ba7031
BlackGear27 <itsblackgear@gmail.com>
2021-05-28 01:30:49 -0400

    new grimestone blocks

---
+++ src/main/java/com/naterbobber/darkerdepths/common/blocks/MossyGrimestoneBlock.java
@@ -0,0 +1,11 @@
+package com.naterbobber.darkerdepths.common.blocks;
+
+import net.minecraft.block.Block;
+
+//<>
+
+public class MossyGrimestoneBlock extends Block {
+    public MossyGrimestoneBlock(Properties properties) {
+        super(properties);
+    }
+}
