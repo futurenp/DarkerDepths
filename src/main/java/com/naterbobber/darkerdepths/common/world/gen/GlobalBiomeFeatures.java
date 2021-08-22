e3c7ee40c224eb12112339d3dd64074fbc1feae6
0rc1nus <mitchellctlui@gmail.com>
2021-08-23 07:41:16 +0800

    - Tweaked glowshroom monster mob spawn rate

--- src/main/java/com/naterbobber/darkerdepths/common/world/gen/GlobalBiomeFeatures.java
+++ src/main/java/com/naterbobber/darkerdepths/common/world/gen/GlobalBiomeFeatures.java
@@ -76,7 +76,7 @@ public class GlobalBiomeFeatures {
 			VanillaBiomeFeatures.addMineables(manager.generation());
 			manager.generation().withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, DDConfiguredFeatures.GLOWSHROOM_CAVE_TERRAIN);
 			//TODO: kill orcinus in-game
-			manager.spawns().withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(DDEntityTypes.GLOWSHROOM_MONSTER.get(), 10, 1, 2));
+			manager.spawns().withSpawner(EntityClassification.MONSTER, new MobSpawnInfo.Spawners(DDEntityTypes.GLOWSHROOM_MONSTER.get(), 50, 1, 2));
 		}
 	}
 
