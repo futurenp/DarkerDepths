5cfb59a43498995950568ea54366d0b841bc175e
0rc1nus <mitchellctlui@gmail.com>
2022-06-24 08:18:06 +0800

    Magma ore fixes

--- src/main/java/com/naterbobber/darkerdepths/data/DDDataGenerator.java
+++ src/main/java/com/naterbobber/darkerdepths/data/DDDataGenerator.java
@@ -20,6 +20,7 @@ public class DDDataGenerator {
         dataGenerator.addProvider(event.includeServer(), new DDRecipeProvider(dataGenerator));
         dataGenerator.addProvider(event.includeServer(), new DDBlockTagsProvider(dataGenerator, existingFileHelper));
         dataGenerator.addProvider(event.includeServer(), new DDItemTagsProvider(dataGenerator, existingFileHelper));
+        dataGenerator.addProvider(event.includeServer(), new DDBiomeTagsProvider(dataGenerator, existingFileHelper));
     }
 
 }
