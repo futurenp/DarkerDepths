7c266c084245e92de96962dd8968027ba7ccb2bb
0rc1nus <mitchellctlui@gmail.com>
2025-05-04 18:23:01 +1000

    Fixed dynamic lighting

--- src/main/java/com/naterbobber/darkerdepths/init/DDEntityTypes.java
+++ src/main/java/com/naterbobber/darkerdepths/init/DDEntityTypes.java
@@ -17,8 +17,8 @@ public class DDEntityTypes {
 
     public static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, DarkerDepths.MODID);
 
-    public static final RegistryObject<EntityType<PetrifiedBoatEntity>> PETRIFIED_BOAT = ENTITY_TYPES.register("petrified_boat", () -> EntityType.Builder.<PetrifiedBoatEntity>of(PetrifiedBoatEntity::new, MobCategory.MISC).sized(1.375F, 0.5625F).build(new ResourceLocation(DarkerDepths.MODID, "petrified_boat").toString()));
-    public static final RegistryObject<EntityType<PetrifiedChestBoatEntity>> PETRIFIED_CHEST_BOAT = ENTITY_TYPES.register("petrified_chest_boat", () -> EntityType.Builder.<PetrifiedChestBoatEntity>of(PetrifiedChestBoatEntity::new, MobCategory.MISC).sized(1.375F, 0.5625F).build(new ResourceLocation(DarkerDepths.MODID, "petrified_chest_boat").toString()));
-    public static final RegistryObject<EntityType<GlowshroomMonsterEntity>> GLOWSHROOM_MONSTER = ENTITY_TYPES.register("glowshroom_monster", () -> EntityType.Builder.of(GlowshroomMonsterEntity::new, MobCategory.MONSTER).sized(1.3964844F, 1.4F).clientTrackingRange(8).build(new ResourceLocation(DarkerDepths.MODID, "glowshroom_monster").toString()));
+    public static final RegistryObject<EntityType<PetrifiedBoatEntity>> PETRIFIED_BOAT = ENTITY_TYPES.register("petrified_boat", () -> EntityType.Builder.<PetrifiedBoatEntity>of(PetrifiedBoatEntity::new, MobCategory.MISC).sized(1.375F, 0.5625F).build(DarkerDepths.id("petrified_boat").toString()));
+    public static final RegistryObject<EntityType<PetrifiedChestBoatEntity>> PETRIFIED_CHEST_BOAT = ENTITY_TYPES.register("petrified_chest_boat", () -> EntityType.Builder.<PetrifiedChestBoatEntity>of(PetrifiedChestBoatEntity::new, MobCategory.MISC).sized(1.375F, 0.5625F).build(DarkerDepths.id("petrified_chest_boat").toString()));
+    public static final RegistryObject<EntityType<GlowshroomMonsterEntity>> GLOWSHROOM_MONSTER = ENTITY_TYPES.register("glowshroom_monster", () -> EntityType.Builder.of(GlowshroomMonsterEntity::new, MobCategory.MONSTER).sized(1.3964844F, 1.4F).clientTrackingRange(8).build(DarkerDepths.id("glowshroom_monster").toString()));
 
 }
---
+++ src/main/java/com/naterbobber/darkerdepths/mixin/BlockLightEngineMixin.java
@@ -0,0 +1,23 @@
+package com.naterbobber.darkerdepths.mixin;
+
+import com.naterbobber.darkerdepths.client.DynamicLightHandler;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.lighting.BlockLightEngine;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(BlockLightEngine.class)
+public class BlockLightEngineMixin {
+
+    @Inject(at = @At("HEAD"), method = "getEmission", cancellable = true)
+    private void DD$getEmission(long sectionPos, BlockState state, CallbackInfoReturnable<Integer> cir) {
+        BlockPos blockPos = BlockPos.of(sectionPos);
+        if (DynamicLightHandler.LIGHT_SOURCES.containsKey(blockPos) && DynamicLightHandler.LIGHT_SOURCES.get(blockPos).shouldKeep) {
+            cir.setReturnValue(10);
+        }
+    }
+
+}
