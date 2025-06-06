1fe5ea823eda420ab83f21948f334b409289e0fb
0rc1nus <mitchellctlui@gmail.com>
2025-06-06 12:45:48 +1000

    Petrified roots can't grow randomly

--- src/main/java/com/naterbobber/darkerdepths/blocks/PetrifiedRootBlock.java
+++ src/main/java/com/naterbobber/darkerdepths/blocks/PetrifiedRootBlock.java
@@ -22,6 +22,11 @@ public class PetrifiedRootBlock extends GrowingPlantHeadBlock {
     }
 
     @Override
+    public boolean isRandomlyTicking(BlockState p_53961_) {
+        return false;
+    }
+
+    @Override
     public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext ctx) {
         Vec3 vec3 = blockState.getOffset(blockGetter, blockPos);
         return super.getShape(blockState, blockGetter, blockPos, ctx).move(vec3.x, 0.0D, vec3.z);
