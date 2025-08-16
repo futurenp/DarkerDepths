2ec5d109d1b06a34268d25f65da7829fcea2225a
0rc1nus <mitchellctlui@gmail.com>
2025-08-16 10:43:01 +1000

    Adjusted creative tabs

--- src/main/java/com/naterbobber/darkerdepths/init/DDItems.java
+++ src/main/java/com/naterbobber/darkerdepths/init/DDItems.java
@@ -22,15 +22,15 @@ public class DDItems {
     public static final RegistryObject<Item> QUICKROPE = ITEMS.register("quickrope", () -> new QuickRopeItem(new Item.Properties().durability(16)));
     public static final RegistryObject<Item> MAGMA_PAD = ITEMS.register("magma_pad", () -> new PlaceOnWaterBlockItem(DDBlocks.MAGMA_PAD.get(), new Item.Properties()));
     public static final RegistryObject<Item> AMBER = createSimpleItem("amber");
+    public static final RegistryObject<Item> VOID_SOUL_REQUIEM = createSimpleItem("void_soul_requiem");
+    public static final RegistryObject<Item> FORSAKEN_BRONZE_SCRAP = createItem("forsaken_bronze_scrap");
     public static final RegistryObject<Item> STILETTO = ITEMS.register("stiletto", () -> new StilettoItem(2, -1.6F, new Item.Properties()));
     public static final RegistryObject<Item> VOID_SOUL_TORCH = ITEMS.register("void_soul_torch", () -> new VoidSoulTorchItem(DDBlocks.VOID_SOUL_TORCH.get(), DDBlocks.WALL_VOID_SOUL_TORCH.get(), new Item.Properties()));
-    public static final RegistryObject<Item> VOID_SOUL_REQUIEM = createSimpleItem("void_soul_requiem");
     public static final RegistryObject<Item> GLOWSHROOM_MONSTER_SPAWN_EGG = ITEMS.register("glowshroom_monster_spawn_egg", () -> new ForgeSpawnEggItem(DDEntityTypes.GLOWSHROOM_MONSTER, 8290688, 8513702, new Item.Properties()));
     public static final RegistryObject<Item> BODY_SNATCHER_SPAWN_EGG = ITEMS.register("body_snatcher_spawn_egg", () -> new ForgeSpawnEggItem(DDEntityTypes.BODY_SNATCHER, 2496017, 16775339, new Item.Properties()));
     public static final RegistryObject<Item> PETRIFIED_BOAT = ITEMS.register("petrified_boat", () -> new DDBoatItem(false, PetrifiedBoatEntity.BoatType.PETRIFIED, new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> PETRIFIED_CHEST_BOAT = ITEMS.register("petrified_chest_boat", () -> new DDBoatItem(true, PetrifiedBoatEntity.BoatType.PETRIFIED, new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> PETRIFIED_SIGN = ITEMS.register("petrified_sign", () -> new SignItem(new Item.Properties().stacksTo(16), DDBlocks.PETRIFIED_SIGN.get(), DDBlocks.PETRIFIED_WALL_SIGN.get()));
-    public static final RegistryObject<Item> FORSAKEN_BRONZE_SCRAP = createItem("forsaken_bronze_scrap");
 
     private static RegistryObject<Item> createItem(String name) {
         return ITEMS.register(name, () -> new Item(new Item.Properties()));
