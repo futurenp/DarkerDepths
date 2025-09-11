17fedf3f25bb5405603c65f48f19453ad8f5b9ea
futurenp <nathantylicki@gmail.com>
2025-09-10 21:30:02 -0500

    paranoia altar configs

--- src/main/java/com/naterbobber/darkerdepths/config/DDConfigs.java
+++ src/main/java/com/naterbobber/darkerdepths/config/DDConfigs.java
@@ -11,6 +11,10 @@ public class DDConfigs {
     public static final ForgeConfigSpec.ConfigValue<Integer> SUPERCHARGE_ATTACK_SPEED;
     public static final ForgeConfigSpec.ConfigValue<Integer> SUPERCHARGE_ATTACK_DAMAGE;
     public static final ForgeConfigSpec.ConfigValue<Boolean> SUPERCHARGE_UNBREAKABLE;
+    public static final ForgeConfigSpec.ConfigValue<Boolean> PARANOIA_ALTAR_EFFECTS_CREATIVE;
+    public static final ForgeConfigSpec.ConfigValue<Integer> PARANOIA_ALTAR_RADIUS_HORIZONTAL;
+    public static final ForgeConfigSpec.ConfigValue<Integer> PARANOIA_ALTAR_RADIUS_VERTICAL;
+
     public static final DDBiomeConfig SANDY_CATACOMBS_CLIMATE;
     public static final DDBiomeConfig GLOWSHROOM_FOREST_CLIMATE;
     public static final DDBiomeConfig MOLTEN_CAVERN_CLIMATE;
@@ -18,7 +22,6 @@ public class DDConfigs {
 
     static {
         BUILDER.push("Biomes");
-
         var sandyCatacombsDefaults = new DDBiomeConfig.Defaults(
                 0.5, 2.0,
                 -1.0, -0.4,
@@ -51,11 +54,10 @@ public class DDConfigs {
                 0.0
         );
         MOLTEN_CAVERN_CLIMATE = DDBiomeConfig.create(BUILDER, "molten_cavern", moltenCavernDefaults);
-
         BUILDER.pop();
 
-        BUILDER.push("Supercharges");
 
+        BUILDER.push("Supercharges");
         SUPERCHARGE_MINUTES = BUILDER.comment("Duration of the supercharge buff in minutes")
                 .defineInRange("supercharge_minutes", 5, 1, 3600);
 
@@ -71,7 +73,15 @@ public class DDConfigs {
                 .defineInRange("supercharge_attack_damage", 20, 0, 1000);
 
         SUPERCHARGE_UNBREAKABLE = BUILDER.comment("Supercharge tool unbreakablity:").define("supercharge_unbreakable", true);
+        BUILDER.pop();
 
+
+        BUILDER.push("Paranoia Altar");
+        PARANOIA_ALTAR_EFFECTS_CREATIVE = BUILDER.comment("Allows players in creative mode to be effected by the Paranoia Altar")
+                .define("paranoia_altar_effects_creative", false);
+
+        PARANOIA_ALTAR_RADIUS_HORIZONTAL = BUILDER.defineInRange("paranoia_altar_horizontal_range", 72, 0, 128);
+        PARANOIA_ALTAR_RADIUS_VERTICAL = BUILDER.defineInRange("paranoia_altar_vertical_range", 8, 0, 128);
         BUILDER.pop();
 
         SPEC = BUILDER.build();
