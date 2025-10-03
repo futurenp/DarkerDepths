66e5d4d8a7554191eb6cec48b7f6d9ddefe9cf24
futurenp <nathantylicki@gmail.com>
2025-10-03 02:45:47 -0500

    Update RequiemCurseRemovalHandler.java

--- src/main/java/com/naterbobber/darkerdepths/events/RequiemCurseRemovalHandler.java
+++ src/main/java/com/naterbobber/darkerdepths/events/RequiemCurseRemovalHandler.java
@@ -23,7 +23,7 @@ public class RequiemCurseRemovalHandler {
             return;
         }
 
-        if (hasCurse(leftStack)) {
+        if (!hasCurse(leftStack)) {
             return;
         }
 
