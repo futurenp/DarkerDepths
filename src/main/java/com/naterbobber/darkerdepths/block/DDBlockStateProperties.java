fb56c4f6e31af16b89a533354fdc4189287537e3
nathan-tylicki <52610746+futurenp@users.noreply.github.com>
2026-02-20 16:30:16 -0600

    Heated Darkslate Blockstates

--- src/main/java/com/naterbobber/darkerdepths/block/DDBlockStateProperties.java
+++ src/main/java/com/naterbobber/darkerdepths/block/DDBlockStateProperties.java
@@ -5,4 +5,5 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
 public class DDBlockStateProperties {
     public static final IntegerProperty CRYSTAL_GROWTH_LEVEL = IntegerProperty.create("crystal_growth_level", 0, 3);
     public static final IntegerProperty MELON_GROWTH_LEVEL = IntegerProperty.create("melon_growth_level", 0, 2);
+    public static final IntegerProperty HEAT_LEVEL = IntegerProperty.create("heat_level", 0, 4);
 }
