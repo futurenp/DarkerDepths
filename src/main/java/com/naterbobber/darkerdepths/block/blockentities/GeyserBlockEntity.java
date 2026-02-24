a2a65c763994c759a18a1513081d0903e8b3d333
futurenp <52610746+futurenp@users.noreply.github.com>
2026-02-24 17:00:35 -0600

    Geyser random bursts

--- src/main/java/com/naterbobber/darkerdepths/block/blockentities/GeyserBlockEntity.java
+++ src/main/java/com/naterbobber/darkerdepths/block/blockentities/GeyserBlockEntity.java
@@ -1,68 +1,50 @@
 package com.naterbobber.darkerdepths.block.blockentities;
 
-import com.naterbobber.darkerdepths.DarkerDepths;
 import com.naterbobber.darkerdepths.block.DDBlockStateProperties;
 import com.naterbobber.darkerdepths.block.custom.GeyserBlock;
 import com.naterbobber.darkerdepths.init.DDBlockEntityTypes;
 import com.naterbobber.darkerdepths.util.DDTags;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.protocol.game.ClientboundSetEntityMotionPacket;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.levelgen.feature.DripstoneUtils;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
 public class GeyserBlockEntity extends BlockEntity {
-    private static final int burstLength = 100;
-    private static final int burstDelay = 100;
-    private int currentBurst = burstLength;
-    private int currentDelay = burstDelay;
-    private BooleanProperty BURSTING = DDBlockStateProperties.BURSTING;
-    private Logger LOGGER = DarkerDepths.LOGGER;
+    private static final BooleanProperty BURSTING = DDBlockStateProperties.BURSTING;
+    private static final String burstDelayTag = "burstDelay";
+    private static final String burstLengthTag = "burstLength";
+    private static final int minBurstLength = 100;
+    private static final int minBurstDelay = 500;
+    private int currentBurstLength = minBurstLength;
+    private int currentBurstDelay = minBurstDelay;
 
     public GeyserBlockEntity(BlockPos pos, BlockState state) {
         super(DDBlockEntityTypes.GEYSER.get(), pos, state);
     }
 
     public void tick(Level level, BlockPos blockPos, BlockState blockState) {
-        if (level.isClientSide()) {
-            return;
-        }
-        System.out.println("burst: " + currentBurst + " delay: " + currentDelay);
-
-
-        if (currentBurst == 0 && isBursting()) {
-            blockState.setValue(BURSTING, false);
-            level.setBlock(blockPos, blockState, 3);
-            currentBurst = burstLength;
-        }
-
-        if(currentDelay == 0 && !isBursting()) {
-            blockState.setValue(BURSTING, true);
-            level.setBlock(blockPos, blockState, 3);
-            currentDelay = burstDelay;
-        }
+        if (level.isClientSide()) return;
 
         if (isBursting()) {
-            currentBurst--;
+            updateBurstLength(level, blockState, blockPos);
         } else {
-            currentDelay--;
+            updateBurstDelay(level, blockState, blockPos);
             return;
         }
 
-
         Direction direction = blockState.getValue(GeyserBlock.FACING);
-
         BlockPos relativePosition;
         BlockState relativeState;
         int boostColumnLength = 6;
@@ -71,19 +53,15 @@ public class GeyserBlockEntity extends BlockEntity {
             relativePosition = blockPos.relative(direction, i + 1);
             relativeState = level.getBlockState(relativePosition);
             if (level.isStateAtPosition(relativePosition, DripstoneUtils::isEmptyOrWaterOrLava) ||
-                    relativeState.getTags().anyMatch(DDTags.Blocks.GEYSER_BYPASSES::equals) ||
-                    (relativeState.hasProperty(BlockStateProperties.LAYERS) && relativeState.getValue(BlockStateProperties.LAYERS) == 1)) {
-                break;
+                    relativeState.getTags().anyMatch(DDTags.Blocks.GEYSER_BYPASSES::equals)) {
+                boostEntities(level, direction, blockPos, relativePosition);
             }
-
-            boostEntities(level, direction, blockPos, relativePosition);
         }
     }
 
     private boolean isBursting() {
         return this.getBlockState().getValue(BURSTING);
     }
-
 
     private static void boostEntities(Level level, Direction direction, BlockPos originPos, BlockPos blockPos) {
         List<Entity> nearbyEntities = level.getEntitiesOfClass(Entity.class, new AABB(blockPos));
@@ -108,4 +86,42 @@ public class GeyserBlockEntity extends BlockEntity {
         }
     }
 
+    private void updateBurstDelay(Level level, BlockState blockState, BlockPos blockPos) {
+        if(currentBurstDelay == 0) {
+            setBursting(level, blockState, blockPos, true);
+            currentBurstDelay = minBurstDelay + (int)(Math.random() * 1000);
+        } else {
+            currentBurstDelay--;
+        }
+        setChanged();
+    }
+
+    private void updateBurstLength(Level level, BlockState blockState, BlockPos blockPos) {
+        if(currentBurstLength == 0) {
+            setBursting(level, blockState, blockPos, false);
+            currentBurstLength = minBurstLength + (int)(Math.random() * 100);
+        } else {
+            currentBurstLength--;
+        }
+        setChanged();
+    }
+
+    private void setBursting(Level level, BlockState blockState, BlockPos blockPos, boolean value) {
+        BlockState newBlockState = blockState.setValue(BURSTING, value);
+        level.setBlock(blockPos, newBlockState, 3);
+    }
+
+    @Override
+    protected void saveAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.saveAdditional(tag, registries);
+        tag.putInt(burstDelayTag, currentBurstDelay);
+        tag.putInt(burstLengthTag, currentBurstLength);
+    }
+
+    @Override
+    protected void loadAdditional(CompoundTag tag, HolderLookup.Provider registries) {
+        super.loadAdditional(tag, registries);
+        currentBurstDelay = tag.getInt(burstDelayTag);
+        currentBurstLength = tag.getInt(burstLengthTag);
+    }
 }
--- src/main/java/com/naterbobber/darkerdepths/compat/DDCompat.java
+++ src/main/java/com/naterbobber/darkerdepths/compat/DDCompat.java
@@ -1,7 +1,5 @@
 package com.naterbobber.darkerdepths.compat;
 
-import com.naterbobber.darkerdepths.DarkerDepths;
-
 public class DDCompat {
     public static final CompatID QUARK = CompatID.createCompatID("quark");
     public static final CompatID NO_MANS_LAND = CompatID.createCompatID("nomansland");
