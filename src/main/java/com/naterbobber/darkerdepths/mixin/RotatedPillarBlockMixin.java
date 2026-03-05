14e6d40c594b85fccb4b34c7bc5134cb824b84d2
futurenp <52610746+futurenp@users.noreply.github.com>
2026-03-05 16:07:19 -0600

    Fix for #21 #22

--- src/main/java/com/naterbobber/darkerdepths/mixin/RotatedPillarBlockMixin.java
+++ src/main/java/com/naterbobber/darkerdepths/mixin/RotatedPillarBlockMixin.java
@@ -10,7 +10,10 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.RotatedPillarBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.neoforged.neoforge.common.Tags;
 import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.stream.Collectors;
 
 @Mixin(RotatedPillarBlock.class)
 public class RotatedPillarBlockMixin extends Block {
@@ -22,21 +25,37 @@ public class RotatedPillarBlockMixin extends Block {
     @Override
     public void onPlace(BlockState blockState, Level level, BlockPos blockPos, BlockState oldState, boolean movedByPiston) {
         super.onPlace(blockState, level, blockPos, oldState, movedByPiston);
+        checkForPetrifiedConversion(level, blockState, blockPos);
+    }
 
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block neighborBlock, BlockPos neighborPos, boolean movedByPiston) {
+        super.neighborChanged(state, level, pos, neighborBlock, neighborPos, movedByPiston);
+        checkForPetrifiedConversion(level, state, pos);
+    }
+
+    private void checkForPetrifiedConversion(Level level, BlockState blockState, BlockPos blockPos) {
         if(!blockState.is(BlockTags.LOGS_THAT_BURN)) return;
+
+        BlockState belowState = level.getBlockState(blockPos.below());
+
+        if(!(belowState.is(Blocks.SOUL_FIRE) || belowState.is(Blocks.SOUL_CAMPFIRE))) return;
 
         Block petrifiedLog;
 
-        if(blockState.is(BlockTags.OVERWORLD_NATURAL_LOGS)) {
+        var tags = blockState.getTags().collect(Collectors.toSet());
+
+        if(tags.contains(BlockTags.OVERWORLD_NATURAL_LOGS)) {
             petrifiedLog = DDBlocks.PETRIFIED_LOG.get();
+        } else if (tags.contains(Tags.Blocks.STRIPPED_LOGS)) {
+            petrifiedLog = DDBlocks.STRIPPED_PETRIFIED_LOG.get();
+        } else if (tags.contains(Tags.Blocks.STRIPPED_WOODS)) {
+            petrifiedLog = DDBlocks.STRIPPED_PETRIFIED_WOOD.get();
         } else {
             petrifiedLog = DDBlocks.PETRIFIED_WOOD.get();
         }
 
-        BlockState belowState = level.getBlockState(blockPos.below());
-        if ((belowState.is(Blocks.SOUL_FIRE) || belowState.is(Blocks.SOUL_CAMPFIRE))) {
-            level.setBlock(blockPos, petrifiedLog.defaultBlockState().setValue(RotatedPillarBlock.AXIS, blockState.getValue(RotatedPillarBlock.AXIS)), 2);
-            level.levelEvent(1501, blockPos, 0);
-        }
+        level.setBlock(blockPos, petrifiedLog.defaultBlockState().setValue(RotatedPillarBlock.AXIS, blockState.getValue(RotatedPillarBlock.AXIS)), 2);
+        level.levelEvent(1501, blockPos, 0);
     }
 }
