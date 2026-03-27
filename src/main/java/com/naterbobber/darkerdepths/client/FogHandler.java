e59da684e76f86930aa048b165a1c9f5ec9ca6ef
nathan-tylicki <52610746+futurenp@users.noreply.github.com>
2026-03-27 12:53:53 -0500

    Clean up fog handler, untested

--- src/main/java/com/naterbobber/darkerdepths/client/FogHandler.java
+++ src/main/java/com/naterbobber/darkerdepths/client/FogHandler.java
@@ -7,28 +7,52 @@ import com.naterbobber.darkerdepths.init.DDMobEffects;
 import com.naterbobber.darkerdepths.util.DDResourceKeys;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.biome.Biome;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.neoforge.client.event.ClientTickEvent;
 import net.neoforged.neoforge.client.event.ViewportEvent;
+import software.bernie.geckolib.util.Color;
+
+import java.util.List;
 
 public class FogHandler {
     private static float paranoiaFactor = 0.0f;
     private static final float TRANSITION_SECONDS = 1.5f;
-    private static float moltenCavernWeight = 0.0f;
-    private static float glowshroomWeight = 0.0f;
-    private static float catacombsWeight = 0.0f;
     private static final float BIOME_TRANSITION_SECONDS = 2.0f;
     private static boolean useBiomeFog = true;
+    private static final BiomeFog MOLTEN_CAVERN = new BiomeFog(
+            DDResourceKeys.Biomes.MOLTEN_CAVERN,
+            Color.ofRGB(0.42F, 0.27F, 0.18F),
+            DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MIN.get(),
+            DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MAX.get()
+    );
+    private static final BiomeFog SANDY_CATACOMBS = new BiomeFog(
+            DDResourceKeys.Biomes.MOLTEN_CAVERN,
+            Color.ofRGB(0.22F, 0.13F, 0.10F),
+            DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MIN.get(),
+            DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MAX.get()
+    );
+    private static final BiomeFog GLOWSHROOM_CAVERN = new BiomeFog(
+            DDResourceKeys.Biomes.MOLTEN_CAVERN,
+            Color.ofRGB(0.16F, 0.34F, 0.24F),
+            DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MIN.get(),
+            DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MAX.get()
+    );
+
+    private static final List<BiomeFog> BIOME_FOG_LIST = List.of(
+            MOLTEN_CAVERN,
+            SANDY_CATACOMBS,
+            GLOWSHROOM_CAVERN
+    );
 
     public static void onClientTick() {
         LocalPlayer player = Minecraft.getInstance().player;
         if (player == null) return;
-
         float ticks = 20.0f;
         float step = 1.0f / (TRANSITION_SECONDS * ticks);
 
@@ -58,22 +82,19 @@ public class FogHandler {
             float biomeStep = 1.0f / (BIOME_TRANSITION_SECONDS * ticks);
             var currentBiome = player.level().getBiome(player.getOnPos());
 
-            boolean inMolten = currentBiome.is(DDResourceKeys.Biomes.MOLTEN_CAVERN);
-            boolean inGlow = currentBiome.is(DDResourceKeys.Biomes.GLOWSHROOM_FOREST);
-            boolean inSand = currentBiome.is(DDResourceKeys.Biomes.SANDY_CATACOMBS);
-
-            moltenCavernWeight = Math.max(0.0f, Math.min(1.0f, moltenCavernWeight + (inMolten ? biomeStep : -biomeStep)));
-            glowshroomWeight = Math.max(0.0f, Math.min(1.0f, glowshroomWeight + (inGlow ? biomeStep : -biomeStep)));
-            catacombsWeight = Math.max(0.0f, Math.min(1.0f, catacombsWeight + (inSand ? biomeStep : -biomeStep)));
+            for(var biome : BIOME_FOG_LIST) {
+                boolean isInBiome = currentBiome.is(biome.getBiome());
+                biome.setWeight(Math.max(0.0f, Math.min(1.0f, biome.getWeight() + (isInBiome ? biomeStep : -biomeStep))));
+            }
         } else {
             resetFog();
         }
     }
 
     private static void resetFog(){
-        moltenCavernWeight = 0.0f;
-        glowshroomWeight = 0.0f;
-        catacombsWeight = 0.0f;
+        for(var biome : BIOME_FOG_LIST) {
+            biome.resetWeight();
+        }
     }
 
     @SubscribeEvent
@@ -83,13 +104,20 @@ public class FogHandler {
         float blue = event.getBlue();
 
         if (DDConfig.CONFIG.ENABLE_BIOME_FOG.get() && useBiomeFog) {
-            float totalWeight = moltenCavernWeight + glowshroomWeight + catacombsWeight;
+            float totalWeight = getTotalBiomeWeight();
+
             if (totalWeight > 0.0f) {
                 float invWeight = Math.max(0.0f, 1.0f - Math.min(1.0f, totalWeight));
 
-                float r = red * invWeight + 0.42f * moltenCavernWeight + 0.16f * glowshroomWeight + 0.22f * catacombsWeight;
-                float g = green * invWeight + 0.27f * moltenCavernWeight + 0.34f * glowshroomWeight + 0.13f * catacombsWeight;
-                float b = blue * invWeight + 0.18f * moltenCavernWeight + 0.24f * glowshroomWeight + 0.10f * catacombsWeight;
+                float r = red * invWeight;
+                float b = red * invWeight;
+                float g = red * invWeight;
+                for(var biome : BIOME_FOG_LIST) {
+                    var color = biome.getWeightedColors();
+                    r += color.getRedFloat();
+                    g += color.getGreenFloat();
+                    b += color.getBlueFloat();
+                }
 
                 float scale = 1.0f / (invWeight + totalWeight);
 
@@ -113,19 +141,20 @@ public class FogHandler {
     @SubscribeEvent
     public void onRenderFog(ViewportEvent.RenderFog event) {
         if (DDConfig.CONFIG.ENABLE_BIOME_FOG.get() && useBiomeFog) {
-            float totalWeight = moltenCavernWeight + glowshroomWeight + catacombsWeight;
+            float totalWeight = getTotalBiomeWeight();
             if (totalWeight > 0.0f) {
                 float invWeight = Math.max(0.0f, 1.0f - Math.min(1.0f, totalWeight));
 
-                float near = invWeight
-                        + DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MIN.get() * moltenCavernWeight
-                        + DDConfig.CONFIG.GLOWSHROOM_FOREST_FOG_MIN.get() * glowshroomWeight
-                        + DDConfig.CONFIG.SANDY_CATACOMBS_FOG_MIN.get() * catacombsWeight;
 
-                float far = invWeight
-                        + DDConfig.CONFIG.MOLTEN_CAVERN_FOG_MAX.get() * moltenCavernWeight
-                        + DDConfig.CONFIG.GLOWSHROOM_FOREST_FOG_MAX.get() * glowshroomWeight
-                        + DDConfig.CONFIG.SANDY_CATACOMBS_FOG_MAX.get() * catacombsWeight;
+                float near = invWeight;
+                for(var biome : BIOME_FOG_LIST) {
+                    near += biome.getWeightedMin();
+                }
+
+                float far = invWeight;
+                for(var biome : BIOME_FOG_LIST) {
+                    far += biome.getWeightedMax();
+                }
 
                 event.setNearPlaneDistance(near);
                 event.setFarPlaneDistance(far);
@@ -145,6 +174,15 @@ public class FogHandler {
             double zoomedFov = defaultFov * (0.9 - (amplifier * 0.05));
             event.setFOV(lerp(defaultFov, zoomedFov, paranoiaFactor));
         }
+    }
+
+    private static float getTotalBiomeWeight() {
+        float total = 0;
+        for(var biome : BIOME_FOG_LIST) {
+            total += biome.getWeight();
+        }
+
+        return total;
     }
 
     private static void checkAndApplyParanoiaFog(ViewportEvent.RenderFog event) {
@@ -217,4 +255,48 @@ public class FogHandler {
     private static double lerp(double start, double end, double factor) {
         return start + factor * (end - start);
     }
+
+    private static class BiomeFog {
+        private final ResourceKey<Biome> biome;
+        private Color color;
+        private float weight;
+        private int minDist;
+        private int maxDist;
+
+        public BiomeFog(ResourceKey<Biome> biome, Color color, int minDist, int maxDist) {
+            this.biome = biome;
+            this.color = color;
+            this.minDist = minDist;
+            this.maxDist = maxDist;
+        }
+
+        public Color getWeightedColors() {
+            return Color.ofRGB(color.getRedFloat() * weight, color.getGreen() * weight, color.getBlueFloat() * weight);
+        }
+
+        public void resetWeight() {
+            weight = 0;
+        }
+
+        public void setWeight(float weight) {
+            this.weight = weight;
+        }
+
+        public float getWeight() {
+            return weight;
+        }
+
+        public ResourceKey<Biome> getBiome() {
+            return biome;
+        }
+
+        public float getWeightedMin() {
+            return weight * minDist;
+        }
+
+        public float getWeightedMax() {
+            return weight * maxDist;
+        }
+    }
+
 }
