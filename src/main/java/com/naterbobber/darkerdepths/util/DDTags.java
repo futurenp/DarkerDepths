eac72d8923856a26b472e85d0d5d3383d716cd50
futurenp <nathantylicki@gmail.com>
2026-04-08 23:31:11 -0500

    Update DDTags.java

--- src/main/java/com/naterbobber/darkerdepths/util/DDTags.java
+++ src/main/java/com/naterbobber/darkerdepths/util/DDTags.java
@@ -7,9 +7,11 @@ import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.material.Fluid;
 
 public class DDTags {
     private record TagFactory<T>(ResourceKey<Registry<T>> registry) {
@@ -39,6 +41,7 @@ public class DDTags {
         private static final TagFactory<Block> TAGS = make(Registries.BLOCK);
 
         public static final TagKey<Block> GEYSER_BOOSTERS = TAGS.create("geyser_boosters");
+        public static final TagKey<Block> GEYSER_ASH_PROVIDERS = TAGS.create("geyser_ash_providers");
         public static final TagKey<Block> GEYSER_BYPASSES = TAGS.create("geyser_bypasses");
         public static final TagKey<Block> HUSKS_SPAWNABLE_ON = TAGS.create("husks_spawnable_on");
         public static final TagKey<Block> VERTICAL_PLANKS = TAGS.create("vertical_planks", DDCompat.QUARK);
@@ -47,6 +50,28 @@ public class DDTags {
         public static final TagKey<Block> STRIPPED_WOODS = TAGS.create("stripped_woods");
         public static final TagKey<Block> STRIPPED_LOGS_FORGE = TAGS.create("stripped_logs", DDCompat.FORGE);
         public static final TagKey<Block> STRIPPED_WOODS_FORGE = TAGS.create("stripped_woods", DDCompat.FORGE);
+        public static final TagKey<Block> CATACOMBS_STRIPE_REPLACEABLE = TAGS.create("catacombs_stripe_replaceable");
+        public static final TagKey<Block> MAGMA_STRIPE_REPLACEABLE = TAGS.create("magma_stripe_replaceable");
+        public static final TagKey<Block> ARID_GROUND = TAGS.create("arid_ground");
+        public static final TagKey<Block> HUGE_GLOWSHROOM_GROWABLE = TAGS.create("huge_glowshroom_growable");
+        public static final TagKey<Block> HEATABLE = TAGS.create("heatable");
+        public static final TagKey<Block> VERY_HIGH_HEAT = TAGS.create("very_high_heat");
+        public static final TagKey<Block> HIGH_HEAT = TAGS.create("high_heat");
+        public static final TagKey<Block> MEDIUM_HEAT = TAGS.create("medium_heat");
+        public static final TagKey<Block> LOW_HEAT = TAGS.create("low_heat");
+        public static final TagKey<Block> HEAT_PROVIDER = TAGS.create("heat_provider");
+        public static final TagKey<Block> GRIME_GROUND = TAGS.create("grime_ground");
+    }
+
+    public static class Fluids {
+        private static final TagFactory<Fluid> TAGS = make(Registries.FLUID);
+
+        public static final TagKey<Fluid> VERY_HIGH_HEAT = TAGS.create("very_high_heat");
+        public static final TagKey<Fluid> HIGH_HEAT = TAGS.create("high_heat");
+        public static final TagKey<Fluid> MEDIUM_HEAT = TAGS.create("medium_heat");
+        public static final TagKey<Fluid> LOW_HEAT = TAGS.create("low_heat");
+        public static final TagKey<Fluid> HEAT_PROVIDER = TAGS.create("heat_provider");
+
     }
 
     public static class Biomes {
@@ -55,5 +80,12 @@ public class DDTags {
         public static final TagKey<Biome> HAS_ROPE_MINE_FOREST = TAGS.create("has_structure/rope_mine_forest");
         public static final TagKey<Biome> HAS_ROPE_MINE_DESERT = TAGS.create("has_structure/rope_mine_desert");
         public static final TagKey<Biome> CATACOMBS = TAGS.create("has_structure/catacombs");
+    }
+
+    public static class EntityTypes {
+        private static final TagFactory<EntityType<?>> TAGS = make(Registries.ENTITY_TYPE);
+
+        public static final TagKey<EntityType<?>> GLOWSHROOM_MONSTER_TARGET = TAGS.create("glowshroom_monster_target");
+        public static final TagKey<EntityType<?>> VOID_SOUL = TAGS.create("void_soul");
     }
 }
