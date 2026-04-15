2dc9aae6bd36821b82e52cc7c65fa3d8a7710110
futurenp <nathantylicki@gmail.com>
2026-04-14 20:55:58 -0500

    Fix #35

--- src/main/java/com/naterbobber/darkerdepths/block/generic/RelationalFenceGateBlock.java
+++ src/main/java/com/naterbobber/darkerdepths/block/generic/RelationalFenceGateBlock.java
@@ -7,7 +7,7 @@ import net.minecraft.world.level.block.state.properties.WoodType;
 public class RelationalFenceGateBlock extends FenceGateBlock implements IRelationalBlock {
     private final Block baseBlock;
     public RelationalFenceGateBlock(Block baseBlock, WoodType type) {
-        super(type, baseBlock.properties());
+        super(type, baseBlock.properties().forceSolidOn());
         this.baseBlock = baseBlock;
     }
 
