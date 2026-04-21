6580dd717c63a9eb4dd48105420580a3754ac0d6
futurenp <52610746+futurenp@users.noreply.github.com>
2026-04-21 17:19:21 -0500

    Darkslate updateByHeat bug

--- src/main/java/com/naterbobber/darkerdepths/block/custom/DarkslateBlock.java
+++ src/main/java/com/naterbobber/darkerdepths/block/custom/DarkslateBlock.java
@@ -39,7 +39,7 @@ public class DarkslateBlock extends RotatedPillarBlock implements HeatableBlock 
 
     @Override
     public BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor level, BlockPos currentPos, BlockPos neighborPos) {
-        if (updatedByHeat(level, state)) {
+        if (updatedByHeat(level, neighborState)) {
             level.scheduleTick(currentPos, this, 10);
         }
         return super.updateShape(state, direction, neighborState, level, currentPos, neighborPos);
